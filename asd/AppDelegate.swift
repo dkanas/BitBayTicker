f54d3c798d503a1d9fa25c2c56d867b644fb6a59
Daniel Kanas <dkanas95@gmail.com>
2017-05-28 19:49:34 +0200

    initial commit & first working version: showing last ethereum sell price and update each 5 seconds

--- asd/AppDelegate.swift
+++ asd/AppDelegate.swift
@@ -10,11 +10,46 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    var timer = Timer()
+    
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.title = "$0.0"
+        statusItem.menu = statusMenu
+        getLastestPrice()
+        self.timer = Timer.scheduledTimer(
+            timeInterval: 5.0,
+            target: self,
+            selector: #selector(self.getLastestPrice),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    func getLastestPrice() {
+        let session = URLSession.shared
+        let url = URL(string: "https://bitbay.net/API/Public/ETHPLN/ticker.json")!
+        let task = session.dataTask(with: url) { (data, _, _) -> Void in
+            do {
+                if let data = data,
+                    let json = try JSONSerialization.jsonObject(with: data) as? [String: Any] {
+                    if let last = json["last"] as? Double {
+                        self.statusItem.title = String(format: "%0.2f zł", arguments: [last])
+                    }
+                }
+            } catch {
+                print("Error deserializing JSON: \(error)")
+            }
+        }
+        task.resume()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
