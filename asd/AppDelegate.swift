0dd1e891937903aefe88ad3e79092b8b2e89f399
Daniel Kanas <dkanas95@gmail.com>
2017-05-28 18:38:57 +0200

    Initial Commit

---
+++ asd/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  asd
+//
+//  Created by user on 28.05.2017.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ asd/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  asd
+//
+//  Created by user on 28.05.2017.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
